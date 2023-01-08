eb9b2b2f0495f8cc41a58f0dd3fc475a3e9fdf9d
ishabellelmisaa <118729310+ishabellelmisaa@users.noreply.github.com>
2023-01-09 02:01:02 +0700

    Create CobaOverride.java

---
+++ CobaOverride.java
@@ -0,0 +1,8 @@
+public class CobaOverride extends StudentRecord {
+    @Override
+    public String getName()
+    {
+        System.out.println("afril juga student loooh....");
+        return name;
+    }
+}
